19bf11b51282bc74b7ad1da10a519b7ed532caa6
Kenny Lee <kenny.lee927@gmail.com>
2021-11-19 06:24:53 -0500

    completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
 const reverseString = function(str) {
+const strArray=str.split("");
+let outputArray = [];
+let outputString = "";
 
-};
+for (i=strArray.length-1; i>=0; i--){
+    outputArray.push(strArray[i])
+}
+
+for (i=0; i<outputArray.length; i++) {
+    outputString += outputArray [i];
+}
+
+return outputString;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
