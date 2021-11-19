de98ed7e68c185fed75385d8e6477e2b7df34744
Kenny Lee <kenny.lee927@gmail.com>
2021-11-19 05:02:19 -0500

    completed exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+let returnString="";
+for (i=0; i<num; i++) {
+    returnString+=string;
+}
+if (num<0){
+    return "ERROR";
+}
+else {
+return returnString;
+}
 };
 
 // Do not edit below this line
