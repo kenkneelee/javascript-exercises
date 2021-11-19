a96b579af07b67b8223ad70ecd7f461d7c064544
Kenny Lee <kenny.lee927@gmail.com>
2021-11-19 08:47:25 -0500

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+    if (first < 0 || second < 0 || typeof first !== "number" || typeof second !== "number") {
+        return "ERROR";
+    }
 
+    if (first > second) {
+        higher = first;
+        lower = second;
+    }
+    else if (second > first) {
+        higher = second;
+        lower = first;
+    }
+    else {
+        return "Numbers are equal!";
+    }
+
+    let oneToHigher = (higher * (higher + 1)) / 2;
+    let oneToLowerMinusOne = ((lower - 1) * lower) / 2;
+    let endSum = oneToHigher - oneToLowerMinusOne;
+    return endSum;
 };
 
 // Do not edit below this line
