9f430b3e734781393ea53ac690033cdf4dd3ce53
Kenny Lee <kenny.lee927@gmail.com>
2021-11-22 12:46:34 -0500

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,18 @@
-const ftoc = function() {
+// C = (F - 32) / 1.8
+// F = (C * 1.8) + 32
 
+const ftoc = function(temp) {
+  let output=((temp-32)/1.8)
+  return Number(output.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let output=((temp*1.8)+32);
+  return Number(output.toFixed(1));
 };
+
+console.log(ftoc(78));
+console.log(ctof(25));
 
 // Do not edit below this line
 module.exports = {
