9995d4f3e57292b02cc3c8e009da8fab9765cd32
Kenny Lee <kenny.lee927@gmail.com>
2021-12-10 21:05:33 -0500

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    newString = string.replace(/[^A-Za-z]/g, "").toLowerCase();
+    return (
+        newString
+            .split("")
+            .reverse()
+            .join("") == newString
+    )
 };
 
 // Do not edit below this line
