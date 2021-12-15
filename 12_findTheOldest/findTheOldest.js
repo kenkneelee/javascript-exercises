12ed7d655125ee75dc256b7eae72104dcedc617c
Kenny Lee <kenny.lee927@gmail.com>
2021-12-14 19:42:28 -0500

    remove console logs from ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,20 +1,3 @@
-const people = [
-    {
-        name: "Carly",
-        yearOfBirth: 2018,
-    },
-    {
-        name: "Ray",
-        yearOfBirth: 1962,
-        yearOfDeath: 2011,
-    },
-    {
-        name: "Jane",
-        yearOfBirth: 1912,
-        yearOfDeath: 1941,
-    },
-]
-
 const findTheOldest = function (array) {
     const addIfAlive = array.map(person => {
         if (!person.yearOfDeath) {
@@ -22,12 +5,9 @@ const findTheOldest = function (array) {
         }
         return person;
     })
-    console.log(addIfAlive)
     const sortAge = addIfAlive.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
     return addIfAlive[0];
 };
-
-console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
 
