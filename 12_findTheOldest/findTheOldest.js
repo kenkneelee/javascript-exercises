1b0b345537278e8055eadeff1970fc13e7bcb283
Kenny Lee <kenny.lee927@gmail.com>
2021-12-14 19:33:06 -0500

    completed exercise 12 FINALLY

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 2018,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
 
+const findTheOldest = function (array) {
+    const addIfAlive = array.map(person => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        return person;
+    })
+    console.log(addIfAlive)
+    const sortAge = addIfAlive.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+    return addIfAlive[0];
 };
 
+console.log(findTheOldest(people));
 // Do not edit below this line
 module.exports = findTheOldest;
+
